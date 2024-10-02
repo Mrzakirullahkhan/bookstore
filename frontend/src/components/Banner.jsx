9674702b5309412e8b9c0700327c1fa5bb3fd0c2
Mr zakir ullah <mrzak802@gmail.com>
2024-10-02 12:06:20 -0700

    comment

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React from "react";
+import banner from "../../src/images/banner.png";
 
 function Banner() {
   return (
     <>
-    <div className='max-w-screen-2xl container m-auto md:px-20 px-4 flex flex-col md:flex-row'>
-        <div className='md:w-1/2 w-full '>left</div>
-        <div className='md:w-1/2 w-full '>right</div>
-    </div>
+      <div className="max-w-screen-2xl container m-auto md:px-20 px-4 flex flex-col md:flex-row">
+        <div className="md:w-1/2 w-full mt-12 md:mt-32 ">
+          <div className="space-y-12">
+            <h1 className="text-4xl font-bold text-black">
+              Hello, welcomes here to learn something
+              <br />
+              <span className="text-pink-500">new everyday!!!</span>
+            </h1>
+            <p className="text-xl text-black">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
+              maxime officiis exercitationem mollitia error ex esse consequatur
+              perferendis quidem tempore, dolorum eligendi, dicta accusantium
+              asperiores culpa, quibusdam aliquam sequi pariatur.
+            </p>
+            <label className="input bg-white input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Email" />
+            </label>
+          </div>
+          <button className="btn mt-5 btn-active btn-secondary">
+            Secondary
+          </button>
+        </div>
+        <div className="md:w-1/2 w-full ">
+          <img src={banner} alt="" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
